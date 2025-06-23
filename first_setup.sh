8565e30487ed2f28f132261f64427c32e52a27a3
Devil10126 <hshshshahwhw0@gmail.com>
2025-06-23 14:43:29 +0530

    Create first_setup.sh

---
+++ first_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo -e "\033[1;31m[+] Installing GhostStrike-X dependencies...\033[0m"
+
+sudo apt update && sudo apt install -y \
+    build-essential \
+    net-tools \
+    curl \
+    gcc \
+    libpcap-dev \
+    libnet1-dev \
+    macchanger \
+    cron \
+    htop \
+    dnsutils \
+    nmap
+
+echo -e "\033[1;32m[✔] Dependencies installed.\033[0m"
+echo -e "\033[1;34m[+] Compiling ghoststrike_x_v4.c...\033[0m"
+
+gcc ghoststrike_x_v4.c -o ghoststrike_x -lpthread
+chmod +x ghoststrike_x
+
+echo -e "\033[1;32m[✔] Build complete. Run with: ./ghoststrike_x\033[0m"
